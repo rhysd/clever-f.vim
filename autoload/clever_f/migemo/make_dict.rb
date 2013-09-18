0d08d9f28777f9b2d6329cabe83ab14c704d2b4e
rhysd <lin90162@yahoo.co.jp>
2013-09-19 03:28:55 +0900

    remove helper script

--- autoload/clever_f/migemo/make_dict.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
-
-%w[ cp932 euc-jp utf-8 ].each do |encoding|
-  [*'a'..'z', *'A'..'Z'].each do |alpha|
-    system "cmigemo -v -w #{alpha} -d /usr/local/share/migemo/#{encoding}/migemo-dict >> #{encoding}.vim && echo >> #{encoding}.vim"
-  end
-end
